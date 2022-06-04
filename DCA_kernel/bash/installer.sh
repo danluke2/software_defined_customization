66f1e0abf9a31f3173a9120db421e0f0674b9351
Daniel Lukaszewski <dflukasz@nps.edu>
2022-06-04 16:52:12 -0700

    Adjust formatting

--- DCA_kernel/bash/installer.sh
+++ DCA_kernel/bash/installer.sh
@@ -4,44 +4,48 @@ set -e
 
 # Init
 PM="/bin/apt" # Package Manager
-CURDIR="$( pwd )"
+CURDIR="$(pwd)"
 
 # ************** STANDARD PARAMS MUST GO HERE ****************
 INSTALLER_MAKEFILE_DIR=/home/vagrant/software_defined_customization/DCA_kernel
 INSTALL_LOCATION=/usr/lib/modules/5.13.0-35-generic/layer4_5
+INCLUDE_DIR=/usr/lib/modules/5.13.0-35-generic/build/include
 CUST_LOCATION=/usr/lib/modules/5.13.0-35-generic/layer4_5/customizations
+GIT_DIR=/home/vagrant/software_defined_customization
 DCA_LOCATION=/usr/lib/modules/5.13.0-35-generic/layer4_5/DCA
 DCA_USER_DIR=/home/vagrant/software_defined_customization/DCA_user
-
-
-# ************** STANDARD PARAMS MUST GO HERE ****************
+# ************** END STANDARD PARAMS ****************
 
 # Force root
-if [[ "$(id -u)" != "0" ]];
-then
-	echo "This script must be run as root" 1>&2
-	exit -1
+if [[ "$(id -u)" != "0" ]]; then
+  echo "This script must be run as root" 1>&2
+  exit -1
 fi
-
 
 # ----------------------------------------------------------------
 # Function for exit due to fatal program error
 #   Accepts 1 argument:
 #     string containing descriptive error message
 # ----------------------------------------------------------------
-error_exit()
-{
+error_exit() {
   echo "${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
   exit 1
 }
-
 
 echo "***************************"
 echo "Installing dependencies"
 # Required build libraries
 $PM -y install python3 python3-pip linux-headers-$(uname -r)
 
+echo "***************************"
+echo "Copying files for module includes"
+cd $INSTALLER_MAKEFILE_DIR
+cp common_structs.h $INCLUDE_DIR
+cp util/printing.h $INCLUDE_DIR
 
+# this just helps for vscode to find file on my mac
+cp common_structs.h $GIT_DIR/no_include
+cp util/printing.h $GIT_DIR/no_include
 
 echo "***************************"
 echo "Making Layer 4.5 modules"
@@ -50,8 +54,6 @@ echo "Making Layer 4.5 modules"
 cd $INSTALLER_MAKEFILE_DIR
 make clean || error_exit "Makefile clean error detected"
 make && make install || error_exit "Makefile error detected"
-
-
 
 # Start layer 4.5 processing
 insmod $INSTALL_LOCATION/layer4_5.ko layer4_5_path="$INSTALL_LOCATION"
@@ -64,13 +66,11 @@ echo "***************************"
 echo "Adding Layer 4.5 to modules config for auto load on boot"
 MODULE_FILE=/etc/modules-load.d/layer4-5.conf
 if test -f "$MODULE_FILE"; then
-    echo "$MODULE_FILE exists."
-		rm $MODULE_FILE
+  echo "$MODULE_FILE exists."
+  rm $MODULE_FILE
 fi
 cp /etc/modules-load.d/modules.conf $MODULE_FILE
-echo "layer4_5" >> $MODULE_FILE
-
-
+echo "layer4_5" >>$MODULE_FILE
 
 # if layer4_5 module insertion succeeds, then build the loader service and enable
 echo "***************************"
@@ -80,19 +80,18 @@ mkdir -p $CUST_LOCATION
 
 cd $CURDIR
 
-
 LOADER_FILE=/etc/systemd/system/layer4_5_loader.service
 #create systemd service for the loader
 #set -o noclobber
 
 if test -f "$LOADER_FILE"; then
-    echo "$LOADER_FILE exists."
-		rm $LOADER_FILE
+  echo "$LOADER_FILE exists."
+  rm $LOADER_FILE
 fi
 
 touch $LOADER_FILE
 
-cat <<EOT >> $LOADER_FILE
+cat <<EOT >>$LOADER_FILE
 [Unit]
 Description=Customization loader systemd service.
 
@@ -106,7 +105,6 @@ EOT
 
 #register loader with systemd
 systemctl enable layer4_5_loader.service
-
 
 echo "***************************"
 echo "Copying DCA files"
