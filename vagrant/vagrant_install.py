dd61374bdf88beeb48f363a326a6b2e0f0139b33
Daniel Lukaszewski <danluke2@yahoo.com>
2024-03-31 21:44:29 -0700

    Updated some formatting and included new option

--- vagrant/vagrant_install.py
+++ vagrant/vagrant_install.py
@@ -1,50 +1,52 @@
-import os
 import subprocess
+import sys
+
 
 def valid_int(val):
-  ''' Verify input is an integer type.'''
-  
-  if val.isnumeric():
-    return val
-  
-  else:
-    valid_int(input('Error: Please provide a valid integer: '))
+    """Verify input is an integer type."""
+
+    if val.isnumeric():
+        return val
+
+    else:
+        valid_int(input("Error: Please provide a valid integer: "))
 
 
-def machine_input(name, box_version, ip_addr, vbox_spec, provider, vmware_spec, memory, cpu):
-    '''Populate and return the vagrantfile cofiguration variables for new machine. '''
-    
-    machines = f'''
+def machine_input(
+    name, box_version, ip_addr, vbox_spec, provider, vmware_spec, memory, cpu
+):
+    """Populate and return the vagrantfile cofiguration variables for new machine."""
+
+    machines = f"""
   # Multiple machines for running experiment modules
   config.vm.define "{name}" do |{name}|
     {name}.vm.box = "squanchy/layer4.5"
     {name}.vm.box_version = "{box_version}"
     {name}.vm.network "private_network", ip: "{ip_addr}" {vbox_spec}
     {name}.vm.provider "{provider}" do |vb|
-      # Display the VirtualBox GUI when booting the machine
       {vmware_spec}
+      # Display the GUI when booting the machine
       vb.gui = true
       vb.memory = {memory}
-      vb.cpus = {cpu}'''
+      vb.cpus = {cpu}"""
 
     return machines
 
 
 def setup_file(setup_path):
-    '''Populate and return the Vagrantfile setup_path for specific VM. '''
-    
-    setup = f'''
-  config.vm.provision :shell, path: "{setup_path}"
-end'''
-    
+    """Populate and return the Vagrantfile setup_path for specific VM."""
+
+    setup = f"""\n\tconfig.vm.provision :shell, path: "{setup_path}"
+end"""
+
     return setup
 
 
 def generate_Vagrantfile():
-    ''' Generate a Vagrantfile. '''
+    """Generate a Vagrantfile."""
 
     ############# STATIC VAGRANTFILE CONTENT #############
-    begin = '''# -*- mode: ruby -*-
+    begin = """# -*- mode: ruby -*-
 # vi: set ft=ruby :
 
 # All Vagrant configuration is done below. The "2" in Vagrant.configure
@@ -60,21 +62,19 @@ Vagrant.configure("2") do |config|
   # Single machine for running sample modules
   # config.vm.box =  "squanchy/layer4.5"
 
-'''
+"""
 
-    vbox_custom = '''
+    vbox_custom = """
       vb.customize ['modifyvm', :id, '--clipboard', 'bidirectional']
       vb.customize ["bandwidthctl", :id, "add", "VagrantLimit", "--type", "network", "--limit", "1000m"]
       vb.customize ["modifyvm", :id, "--nicbandwidthgroup2", "VagrantLimit"]
       #vram setting helps windows 11 machines without necessary processing power
       vb.customize ["modifyvm", :id, "--vram", "128"]
       vb.customize ["modifyvm", :id, "--accelerate3d", "on"]
-    end
-  end
-  
-  '''
-    
-    comments = '''
+  """
+
+    comments = """
+
   # Disable automatic box update checking. If you disable this, then
   # boxes will only be checked for updates when the user runs
   # `vagrant box outdated`. This is not recommended.
@@ -127,109 +127,117 @@ Vagrant.configure("2") do |config|
   # config.vm.provision "shell", inline: <<-SHELL
   #   apt-get update
   #   apt-get install -y apache2
-  # SHELL'''
-    
-    
-    ############# INQUIRY VARIABLES #############
-    vm_type = int(valid_int(input("1) Virtualbox \n2) VmWare \nSelect your virtual machine option above (1 or 2): ")))
-    vm_num = int(valid_int(input("How many machines would you like to activate?: ")))
-    
-    memory = valid_int(input('Enter memory value (MB) in integer (Recommend 8192): '))
-    cpu = valid_int(input('Enter CPU count in integer (Recommend 2):'))
+  # SHELL
+  """
 
-    
+    ############# INQUIRY VARIABLES #############
+    vm_type = int(
+        valid_int(
+            input(
+                "1) Virtualbox (Intel) \n2) VmWare (ARM, M-Series Mac) \n3) Other (e.g., UTM) \nSelect your virtual machine option above (e.g., 1): "
+            )
+        )
+    )
+    # Special case for now since only Virtualbox for Intel and VMWare for ARM are supported
+    if vm_type == 3:
+        print(
+            "Other virtual machine options are not supported at this time. Please refer to manual installation instructions."
+        )
+        sys.exit()
+
+    # Future Feature: Add support for default values
+    vm_num = int(valid_int(input("How many machines would you like to activate?: ")))
+    memory = valid_int(input("Enter memory value (MB) in integer (Recommend 8192): "))
+    cpu = valid_int(input("Enter CPU count in integer (Recommend 2):"))
+
     ############# VAGRANTFILE GENERATION #############
-        
+
     # Variables tailored to vm_type
     if vm_type == 1:
         provider = "virtualbox"
         box_version = "1.2"
-        vbox_spec = ''', virtualbox__intnet: "layer", nic_type: "virtio" '''
-        vmware_spec = ""      # Not required for vbox
+        vbox_spec = """, virtualbox__intnet: "layer", nic_type: "virtio" """
+        vmware_spec = vbox_custom
         setup_path = "setup.sh"
-        # file_path = os.path.join(os.getcwd(), "vagrant", "Vagrantfile")
-        # vm_num += 1
-                
+
     elif vm_type == 2:
         provider = "vmware_fusion"
         box_version = "1.3"
-        vbox_spec = ""      # Not required for vmware
+        vbox_spec = ""  # Not required for vmware
         vmware_spec = "vb.linked_clone = false"
         setup_path = "setup_vmware.sh"
-        # file_path = os.path.join(os.getcwd(), "vagrant_vmware", "Vagrantfile")
-        
-    # file_path = os.path.join(os.getcwd(), "vagrant_test", "Vagrantfile")
-    file_path = "Vagrantfile"
-    
-    # Vagrantfile beginning
-    with open(file_path, 'w') as file:
-        file.write (begin)
 
+    file_path = "Vagrantfile"
+
+    # Vagrantfile beginning
+    with open(file_path, "w") as file:
+        file.write(begin)
 
     # Appending number of machines
     for i in range(vm_num):
         # Update IP Address
         ip_addr = "10.0.0." + str(20 + (i * 10))
-        
+
         # Update name for each machine
         if i == 0:
             name = "server"
-        else: 
+        else:
             name = "client" + str(i)
-        
-        #Appending machine config
-        with open(file_path, 'a') as file:
-            file.write(machine_input(name, box_version, ip_addr, vbox_spec, provider, vmware_spec, memory, cpu))
-        
-        if vm_type == 1:
-            with open (file_path, 'a') as file:
-                file.write(vbox_custom)
-                
-        elif vm_type == 2:
-            with open(file_path, 'a') as file:
-                file.write(f'''
-    end
-    {name}.vm.synced_folder "../../software_defined_customization", "/home/vagrant/software_defined_customization", owner: "vagrant", group: "vagrant", automount: true
-  end''')
-                
-    # Appending the comments of Vagrantfile
-    with open(file_path, 'a') as file:
-        file.write(comments)
-    
-    if vm_type == 1:
-        with open(file_path, 'a') as file:
-            file.write('''
-  config.vm.synced_folder "../../software_defined_customization", "/home/vagrant/software_defined_customization"''')
-        # config.vm.synced_folder "../../software_defined_customization", "/home/vagrant/software_defined_customization"
-        # config.vm.synced_folder "../../software_defined_customization", "/home/vagrant/software_defined_customization", owner: "vagrant", group: "vagrant"
 
-    # Appending setup.sh path            
-    with open(file_path, 'a') as file:
+        # Appending machine config
+        with open(file_path, "a") as file:
+            file.write(
+                machine_input(
+                    name,
+                    box_version,
+                    ip_addr,
+                    vbox_spec,
+                    provider,
+                    vmware_spec,
+                    memory,
+                    cpu,
+                )
+            )
+            file.write("""\n\t\tend\n\tend\n\n""")
+
+        # if vm_type == 1:
+        #     with open(file_path, "a") as file:
+        #         file.write(vbox_custom)
+
+    # Appending the comments of Vagrantfile
+    with open(file_path, "a") as file:
+        file.write(comments)
+
+    with open(file_path, "a") as file:
+        file.write(
+            """\n\tconfig.vm.synced_folder "../../software_defined_customization", "/home/vagrant/software_defined_customization", owner: "vagrant", group: "vagrant", automount: true\n"""
+        )
+
+    # Appending setup.sh path
+    with open(file_path, "a") as file:
         file.write(setup_file(setup_path))
-        
-        
+
+
 def vagrant_up():
-    '''Prompt user and execute vagrant up. '''
-    
+    """Prompt user and execute vagrant up."""
+
     # Ask the user if they want to proceed with 'vagrant up'
     proceed = input("Do you want to proceed with 'vagrant up'? (y/n): ").lower()
-    
-    if proceed != 'y':
+
+    if proceed != "y":
         print("Not running vagrant up.")
-        exit
-        
+        sys.exit()
+
     else:
-        # subprocess.run (['cd', 'vagrant_test'], check = True)
         print("*** Initiating Vagrant UP ***")
         try:
-            subprocess.run(['vagrant', 'up'], check = True)
-            
+            subprocess.run(["vagrant", "up"], check=True)
+
         except subprocess.CalledProcessError as e:
             print(f"Error: {e}")
 
 
 if __name__ == "__main__":
     generate_Vagrantfile()
-    print('Vagrantfile Generated')
+    print("Vagrantfile Generated")
     vagrant_up()
-    
