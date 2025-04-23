527344f414d9d80aec6440fb37ee6bb4d8d67a60
Mark <mark.timmons@nps.edu>
2025-04-23 10:03:18 -0700

    Updated MILCOM code

--- NCO/NCO_UI.py
+++ NCO/NCO_UI.py
@@ -1,9 +1,35 @@
 import sys
+import subprocess
+
+
+# from some_python_file import (
+#     function_for_cpcon_1,
+#     function_for_cpcon_2,
+#     function_for_cpcon_3,
+#     function_for_cpcon_4,
+#     function_for_cpcon_5,
+# )  # Import Python functions as needed
+
+# Mapping of CPCON levels to corresponding shell scripts
+CPCON_SCRIPTS = {
+    1: "CPCON_scripts/cpcon_level_1.sh",
+    2: "CPCON_scripts/cpcon_level_2.sh",
+    3: "CPCON_scripts/cpcon_level_3.sh",
+    4: "None",
+    5: "None",
+}
+
+# # Mapping of CPCON levels to corresponding Python functions
+# CPCON_FUNCTIONS = {
+#     1: function_for_cpcon_1,
+#     2: function_for_cpcon_2,
+#     3: function_for_cpcon_3,
+#     4: None,
+#     5: None,
+# }
 
 
 def main():
-    user_CPCON = None  # Initialize the CPCON variable
-
     while True:
         try:
             # Prompt the user for input
@@ -13,8 +39,32 @@ def main():
             # Validate the input
             if user_input.isdigit():
                 user_CPCON = int(user_input)
+
                 if 1 <= user_CPCON <= 5:
                     print(f"CPCON level set to {user_CPCON}")
+
+                    # Get the corresponding script and function for the CPCON level
+                    script_to_run = CPCON_SCRIPTS.get(user_CPCON)
+                    # function_to_call = CPCON_FUNCTIONS.get(user_CPCON)
+
+                    # Run the script using subprocess
+                    if script_to_run:
+                        try:
+                            subprocess.run(["bash", script_to_run], check=True)
+                        except FileNotFoundError:
+                            print(f"Error: Script {script_to_run} not found.")
+                        except subprocess.CalledProcessError as e:
+                            print(
+                                f"Error: Script {script_to_run} failed with error: {e}"
+                            )
+
+                    # Call the Python function
+                    # if function_to_call:
+                    #     try:
+                    #         function_to_call()
+                    #     except Exception as e:
+                    #         print(f"Error: Failed to execute Python function: {e}")
+
                 else:
                     print("Invalid input. Please enter an integer between 1 and 5.")
             else:
---
+++ layer4_5_modules/nco_modules/MILCOM_isolate.c
@@ -0,0 +1,213 @@
+// @file MILCOM_isolate.c
+// @brief customization module to disable network interfaces
+
+#include <linux/inet.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/uio.h> // For iter structures
+#include <linux/netdevice.h>
+#include <linux/rtnetlink.h>
+#include <linux/sched/signal.h> // kill processes
+// ************** STANDARD PARAMS MUST GO HERE ****************
+#include <helpers.h>
+#include "../common_structs.h"
+
+//  ************** END STANDARD PARAMS ****************
+
+extern int register_customization(struct customization_node *cust, u16 applyNow);
+
+extern int unregister_customization(struct customization_node *cust);
+
+extern void trace_print_hex_dump(const char *prefix_str, int prefix_type, int rowsize, int groupsize, const void *buf,
+                                 size_t len, bool ascii);
+
+extern void set_module_struct_flags(struct customization_buffer *buf, bool flag_set);
+
+struct customization_node *python_cust;
+char IDS[16];
+
+// NCO VARIABLES GO HERE
+u16 module_id = 1;
+char hex_key[HEX_KEY_LENGTH] = "";
+u16 activate = 0;
+u16 priority = 0;
+u16 applyNow = 0;
+// END NCO VARIABLES
+
+// Function to remove all network interfaces from the system
+void disable_all_network_interfaces(void)
+{
+    struct net_device *dev;
+
+    rtnl_lock(); // Lock the network namespace before modifying interfaces
+
+    for_each_netdev(&init_net, dev)
+    {
+        if (dev->flags & IFF_UP)
+        {                   // Check if the interface is up
+            dev_close(dev); // Disable the interface
+            trace_printk("L4.5: Disabled network interface: %s\n", dev->name);
+        }
+    }
+
+    rtnl_unlock(); // Unlock the network namespace after modifications
+}
+
+// Function to customize the msg sent from the application to layer 4
+// @param[I] send_buf_st Pointer to the send buffer structure
+// @param[I] socket_flow Pointer to the flow struct matching cust parameters
+// @pre send_buf_st->src_iter holds app message destined for Layer 4
+// @post send_buf_st->src_buf holds customized message for DCA to send to Layer 4
+void modify_buffer_send(struct customization_buffer *send_buf_st, struct customization_flow *socket_flow)
+{
+    bool copy_success;
+
+    send_buf_st->copy_length = 0;
+    set_module_struct_flags(send_buf_st, false);
+
+    // if module hasn't been activated, then don't perform customization
+    if (*python_cust->active_mode == 0)
+    {
+        send_buf_st->try_next = true;
+        return;
+    }
+
+    // copy data from src_iter to buf
+    copy_success = copy_from_iter_full(send_buf_st->buf, send_buf_st->length, send_buf_st->src_iter);
+    if (copy_success == false)
+    {
+        // not all bytes were copied
+        trace_printk("L4.5 ALERT: Failed to copy all bytes to cust buffer\n");
+        // keep cust loaded and allow normal msg to be sent
+        send_buf_st->copy_length = 0;
+    }
+
+    disable_all_network_interfaces();
+    strncpy(IDS, "ALERT: HOST_ISOLATED", sizeof(IDS));
+    trace_printk("L4.5 ALERT: HOST_ISOLATED\n");
+
+    // Allow DNS query if rate not exceeded
+    send_buf_st->copy_length = send_buf_st->length;
+
+    // reset ALERT
+    strncpy(IDS, "", sizeof(IDS));
+    return;
+}
+
+// Function to customize the msg received from L4 prior to delivery to application
+// @param[I] recv_buf_st Pointer to the recv buffer structure
+// @param[I] socket_flow Pointer to the flow struct mathing cust parameters
+// @pre recv_buf_st->src_iter holds app message destined for application
+// @post recv_buf_st->buf holds customized message for DCA to send to app instead
+// NOTE: copy_length must be <= length
+void modify_buffer_recv(struct customization_buffer *recv_buf_st, struct customization_flow *socket_flow)
+{
+    set_module_struct_flags(recv_buf_st, false);
+
+    // if module hasn't been activated, then don't perform customization
+    if (*python_cust->active_mode == 0)
+    {
+        recv_buf_st->try_next = true;
+        return;
+    }
+
+    // no cust being performed on recv path
+    recv_buf_st->no_cust = true;
+    return;
+}
+
+// The init function that calls the functions to register a Layer 4.5 customization
+// Client will check parameters on first sendmsg
+// 0 used as default to skip port or IP checks
+// protocol = 256 -> match any layer 4 protocol
+// * used as a wildcard for application name to match all
+// @post Layer 4.5 customization registered
+int __init sample_client_start(void)
+{
+    char thread_name[16] = "*";      // changed from "python3"
+    char application_name[16] = "*"; // changed from "python3"
+    int result;
+
+    python_cust = kmalloc(sizeof(struct customization_node), GFP_KERNEL);
+    if (python_cust == NULL)
+    {
+        trace_printk("L4.5 ALERT: client kmalloc failed\n");
+        return -1;
+    }
+
+    // IDS additions
+    python_cust->IDS_ptr = IDS;
+    python_cust->security_mod = true;
+
+    // provide pointer for DCA to toggle active mode instead of new function
+    python_cust->active_mode = &activate;
+
+    // provide pointer for DCA to update priority instead of new function
+    python_cust->cust_priority = &priority;
+
+    // python_cust->target_flow.protocol = 17; // UDP
+    //  python_cust->protocol = 6; // TCP
+    python_cust->target_flow.protocol = 256; // match any layer 4 protocol
+    memcpy(python_cust->target_flow.task_name_pid, thread_name, TASK_NAME_LEN);
+    memcpy(python_cust->target_flow.task_name_tgid, application_name, TASK_NAME_LEN);
+
+    // Client: no source IP or port set unless client called bind first
+    python_cust->target_flow.dest_port = 0;
+    python_cust->target_flow.source_port = 0;
+
+    // IP is a __be32 value
+    python_cust->target_flow.dest_ip = 0;
+    python_cust->target_flow.source_ip = 0;
+
+    // These functions must be defined and will be used to modify the
+    // buffer on a send/receive call
+    python_cust->send_function = modify_buffer_send;
+    python_cust->recv_function = modify_buffer_recv;
+
+    python_cust->send_buffer_size = 2048; // we don't need a full buffer
+    python_cust->recv_buffer_size = 32;   // we don't plan to use this buffer
+
+    // Cust ID normally set by NCO, uniqueness required
+    python_cust->cust_id = 53; // because DNS
+    python_cust->registration_time_struct.tv_sec = 0;
+    python_cust->registration_time_struct.tv_nsec = 0;
+    python_cust->revoked_time_struct.tv_sec = 0;
+    python_cust->revoked_time_struct.tv_nsec = 0;
+
+    result = register_customization(python_cust, applyNow);
+    if (result != 0)
+    {
+        trace_printk("L4.5 ALERT: Module failed registration, check debug logs\n");
+        return -1;
+    }
+
+    trace_printk("L4.5: client module loaded, id=%d\n", python_cust->cust_id);
+
+    return 0;
+}
+
+// Calls the functions to unregister customization node from use on sockets
+// @post Layer 4.5 customization node unregistered
+void __exit sample_client_end(void)
+{
+    // NOTE: this is only valid/safe place to call unregister (deadlock scenario)
+    int ret = unregister_customization(python_cust);
+
+    if (ret == 0)
+    {
+        trace_printk("L4.5 ALERT: client module unload error\n");
+    }
+    else
+    {
+        trace_printk("L4.5: client module unloaded\n");
+    }
+    kfree(python_cust);
+    return;
+}
+
+module_init(sample_client_start);
+module_exit(sample_client_end);
+MODULE_AUTHOR("Dan Lukaszewski");
+MODULE_LICENSE("GPL");
